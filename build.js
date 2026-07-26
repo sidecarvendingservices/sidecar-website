03dfb643e34c8b7f143f467f3c58c90c5219dfab
sidecarvendingservices <support@sidecarservices.com>
2026-07-26 15:15:49 -0400

    sandy springs

--- build.js
+++ build.js
@@ -47,6 +47,7 @@ const SITEMAP_PRIORITY = {
   'smart-vending-machine.html': '0.7',
   'switching-vending-companies.html': '0.7',
   'vending-company-comparison.html': '0.7',
+  'vending-machines-sandy-springs-ga.html': '0.7',
   'careers.html': '0.3',
   'privacy.html': '0.2',
   'blog.html': '0.6',
